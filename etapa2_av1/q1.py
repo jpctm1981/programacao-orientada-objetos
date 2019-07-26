b82aa8b36e655fc2b57040df9eee0a81df24f11a
Joao Paulo <jpctm1981@gmail.com>
2019-07-25 21:31:42 -0300

    Questão 1 concluída.

--- etapa2_av1/q1.py
+++ etapa2_av1/q1.py
@@ -1,4 +1,26 @@
 #Aluno: João Paulo Costa Cordeiro
 #Matrícula: 20141143000384
 
-print("Jogo de Dados")
+import random
+
+print("Rolando os dados...")
+print("Vamos rolar um dado de 6 lados com valores de 1 até 6!!!")
+print("Tente acertar...")
+
+print("Rolando o dado...")
+r = (random.randrange(1,6))
+print("Informe um número:")
+n = (int(input()))
+
+while n!=r:
+
+	print("Tente outra vez...")
+	print("Informe um número:")
+	n = (int(input()))	
+	
+
+else:
+	print("Parabéns!!! Você acertou!!!")
+
+
+
